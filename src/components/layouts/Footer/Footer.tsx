bd8293058933b33c049adc412ae9b0272b02ee2b
Duck1s <joeri.breedveld123@gmail.com>
2023-02-08 17:59:55 +0100

    feat(footer): change some footer things but not finished yet

--- src/components/layouts/Footer/Footer.tsx
+++ src/components/layouts/Footer/Footer.tsx
@@ -5,10 +5,7 @@ import { FiGithub, FiLinkedin, FiMail } from "react-icons/fi";
 const Footer = () => {
   return (
     <footer className="flex flex-col gap-12 bg-bgSecondary py-16">
-      <section className="page-width flex items-center justify-center">
-        <h3>Neem contact op</h3>
-      </section>
-      <section className="page-width flex flex-col gap-4">
+      <section className="page-width flex gap-4">
         <div className="default-rounding flex w-full items-center justify-center border border-gray-200 bg-white py-6 px-6">
           <p>+31 6 80056004</p>
         </div>
