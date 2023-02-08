a088dbe72a5b277c4d7314d5e3a8ef284702e497
Duck1s <joeri.breedveld123@gmail.com>
2023-02-08 18:11:31 +0100

    feat(hero): change font sizes

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -7,8 +7,8 @@ const Hero = () => {
     <section className="flex min-h-screen flex-col items-center justify-center bg-bgSecondary py-16 pt-32 md:pt-36">
       <section className="flex flex-col items-center justify-center gap-16">
         <section className="flex flex-col items-center justify-center gap-8">
-          <h1>Joeri Breedveld</h1>
-          <h3 className="font-normal">Full Stack Developer</h3>
+          <h1 className="text-6xl">Joeri Breedveld</h1>
+          <h3 className="text-3xl font-normal">Full Stack Developer</h3>
         </section>
         <Button variant="secondary">Contact</Button>
       </section>
