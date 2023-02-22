60c5040b48c0779737ea65e741eed58e98b79b35
Duck1s <joeri.breedveld123@gmail.com>
2023-02-22 21:55:42 +0100

    feat(hero): change hero styles and page width style

--- src/components/sections/Hero/Hero.tsx
+++ src/components/sections/Hero/Hero.tsx
@@ -4,15 +4,17 @@ import Button from "../../common/Button/Button";
 // Functions
 const Hero = () => {
   return (
-    <section className="page-width section-py flex min-h-screen flex-col justify-center gap-12">
-      <section className="flex flex-col gap-4">
+    <section className="section-py flex min-h-screen w-full flex-col justify-center gap-12 bg-bgSecondary">
+      <section className="page-width flex flex-col gap-4">
         <h1>Frontend Developer</h1>
         <p>
           I like to make clean and structured websites for the ultimate user
           experience.
         </p>
       </section>
-      <Button variant="secondary">Contact</Button>
+      <section className="page-width">
+        <Button variant="secondary">Contact</Button>
+      </section>
     </section>
   );
 };
