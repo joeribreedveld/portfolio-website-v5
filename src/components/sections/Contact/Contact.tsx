a4823d8f107418a59f26ed3142b5fb5883d5c5bc
Duck1s <joeri.breedveld123@gmail.com>
2023-02-23 11:42:21 +0100

    feat(contact): make contact section

---
+++ src/components/sections/Contact/Contact.tsx
@@ -0,0 +1,31 @@
+// Imports
+import Link from "next/link";
+
+// Functions
+const Contact = () => {
+  return (
+    <section className="section-py flex flex-col gap-8">
+      <section className="page-width">
+        <h2>Contact</h2>
+      </section>
+      <section className="page-width flex flex-col gap-6">
+        <p>Get in touch for more information</p>
+        <ul className="flex flex-col gap-4">
+          <li>
+            <Link href="/" className="text-gray-500 underline">
+              joeri.breedveld123@gmail.com
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className="text-gray-500 underline">
+              +31 6 80056004
+            </Link>
+          </li>
+        </ul>
+      </section>
+    </section>
+  );
+};
+
+// Exports
+export default Contact;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { type NextPage } from "next";
 import Hero from "../components/sections/Hero/Hero";
 import About from "../components/sections/About/About";
 import Projects from "../components/sections/Projects/Projects";
+import Contact from "../components/sections/Contact/Contact";
 
 // Functions
 const Home: NextPage = () => {
@@ -11,6 +12,7 @@ const Home: NextPage = () => {
       <Hero />
       <About />
       <Projects />
+      <Contact />
     </>
   );
 };
