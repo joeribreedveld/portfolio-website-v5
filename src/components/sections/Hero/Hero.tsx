b19c5affc6ba05f7913d8b4411884404af91f018
Duck1s <joeri.breedveld123@gmail.com>
2023-03-08 14:23:48 +0100

    feat(design): change some main styling thigns and hero content

--- src/components/sections/Hero/Hero.tsx
+++ src/components/sections/Hero/Hero.tsx
@@ -7,14 +7,16 @@ const Hero = () => {
   return (
     <section className="section-py flex min-h-screen w-full flex-col justify-center gap-16 bg-bgSecondary">
       <section className="page-width flex flex-col gap-8">
-        <h1>Frontend Developer</h1>
+        <h1>
+          Frontend Developer who <br /> loves learning and challenge
+        </h1>
         <h3 className="font-primary font-normal">
-          I like to make clean and structured websites for the ultimate user
-          experience.
+          I like to build clean and structured websites for the ultimate user
+          experience..
         </h3>
       </section>
       <section className="page-width">
-        <Button variant="secondary">Reach out to me</Button>
+        <Link href="/contact">Get to know me</Link>
       </section>
     </section>
   );
